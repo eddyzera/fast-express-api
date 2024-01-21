d1285def474c13e6bedf604dfcd03afdcf6b7a70
eddyzera <esilva644@gmail.com>
2024-01-21 11:27:28 -0300

    refactor: not allowed authenticate with wrong email

--- src/services/userServices/__test__/authenticateService.test.ts
+++ src/services/userServices/__test__/authenticateService.test.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeEach } from 'vitest'
 import { AuthenticateService } from '@/services/userServices/authenticateService'
 import { InMemoryUsersRepository } from '@/test/inMemoryDataBase/inMemoryUserRepository'
 import { userObj } from '@/test/mocks/userObj'
+import { hash } from 'bcryptjs'
 
 let userRepository: InMemoryUsersRepository
 let sut: AuthenticateService
@@ -12,12 +13,29 @@ describe('AuthenticateService', () => {
     sut = new AuthenticateService(userRepository)
   })
   it('should be able to authenticate', async () => {
-    await userRepository.create({ ...userObj, password_hash: userObj.password })
+    await userRepository.create({
+      ...userObj,
+      password_hash: await hash(userObj.password, 6),
+    })
     const { user } = await sut.execute({
       email: userObj.email,
       password: userObj.password,
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await userRepository.create({
+      ...userObj,
+      password_hash: await hash(userObj.password, 6),
+    })
+
+    await expect(
+      sut.execute({
+        email: 'john_doe-invalid@example.com',
+        password: userObj.password,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
