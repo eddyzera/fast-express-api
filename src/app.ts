a9139afc110cb70911ab6cc3ee6663e1ac513c22
eddyzera <esilva644@gmail.com>
2024-01-21 09:52:43 -0300

    feat: creating error handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from '@/env'
 
 export const app = fastify()
+
+app.setErrorHandler((error, _, response) => {
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: here we should log to an external tool like DataDog
+  }
+
+  return response.status(500).send({ message: 'Internal Server Error' })
+})
