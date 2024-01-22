e4f20bb322bc23f82cd20e8ded655c5af1475316
eddyzera <esilva644@gmail.com>
2024-01-22 12:08:13 -0300

    test: should be able to create a new product

--- src/services/productServices/__test__/createProductServices.test.ts
+++ src/services/productServices/__test__/createProductServices.test.ts
@@ -11,7 +11,7 @@ describe('CreateProductService', () => {
     productRepository = new InMemoryProductRepository()
     sut = new CreateProductServices(productRepository)
   })
-  it('should be able to register', async () => {
+  it('should be able to create a new product', async () => {
     const { product } = await sut.execute(productObj)
 
     expect(product.id).toEqual(expect.any(String))
